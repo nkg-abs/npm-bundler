d70b54032d6bbb74840c8b6fff7b3ea097844284
nkg-abs <nkg@abs.in>
2023-04-15 13:35:37 +0530

    Restructed.

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,13 @@
 const path = require('path');
-const glob = require('glob');
+const { prepareEntry } = require('./lib/webpackManager');
+const config = require('./lib/config');
 const nodeExternals = require('webpack-node-externals');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-const cwd = './src';
-const extensions = ['.js'];
-const ignore = [
-	'**/*.test.js',
-	'**/*.spec.js',
-	'**/setupTests.js',
-];
-const entry = glob
-	.sync(`**/*{${ extensions.join(',') }}`, { cwd, ignore })
-	.reduce((acc, filePath) => ({
-		...acc,
-		[path.parse(filePath).name]: `${ cwd }/${ filePath }`,
-	}), {});
 
 module.exports = {
 	mode: 'production',
 	externals: [nodeExternals()],
-	entry: entry,
+	entry: prepareEntry(config),
 	plugins: [new CleanWebpackPlugin()],
 	module: {
 		rules: [
